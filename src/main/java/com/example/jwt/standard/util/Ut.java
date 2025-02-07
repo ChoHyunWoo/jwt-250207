e5db087869c1db75be2b65c51b9d0d04624b9081
ChoHyunWoo <gusdndlek12@naver.com>
2025-02-07 14:39:31 +0900

    access token

---
+++ src/main/java/com/example/jwt/standard/util/Ut.java
@@ -0,0 +1,39 @@
+package com.example.jwt.standard.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+public class Ut {
+    public static class Json {
+
+        private static final ObjectMapper objectMapper = new ObjectMapper();
+
+        public static String toString(Object obj) {
+            try {
+                return objectMapper.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static class Jwt {
+        public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
+            Date issuedAt = new Date();
+            Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+            String jwt = Jwts.builder()
+                    .setClaims(claims)
+                    .setIssuedAt(issuedAt)
+                    .setExpiration(expiration)
+                    .signWith(secretKey, SignatureAlgorithm.HS256)
+                    .compact();
+            return jwt;
+        }
+    }
+}
---
+++ src/test/java/com/example/jwt/AuthTokenServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.jwt;
+
+import com.example.jwt.domain.member.member.entity.Member;
+import com.example.jwt.domain.member.member.service.AuthTokenService;
+import com.example.jwt.domain.member.member.service.MemberService;
+import com.example.jwt.standard.util.Ut;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Key;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class AuthTokenServiceTest {
+
+    @Autowired
+    private AuthTokenService authTokenService;
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("AuthTokenService 생성")
+    void init() {
+        assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jwt 생성")
+    void createToken() {
+        int expireSeconds = 60 * 60 * 24 * 365;
+        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+
+        String jwt = Ut.Jwt.createToken(secretKey, expireSeconds, Map.of("name", "john", "age", 23));
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
+    }
+
+    @Test
+    @DisplayName("access token 생성")
+    void accessToken() {
+        // jwt -> access token jwt
+        Member member = memberService.findByUsername("user1").get();
+        String accessToken = authTokenService.genAccessToken(member);
+        assertThat(accessToken).isNotBlank();
+        System.out.println("accessToken = " + accessToken);
+    }
+}
